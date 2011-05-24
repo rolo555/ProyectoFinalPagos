10f0797ca64c25e99ca6dcdbfabf22c7cd4fa45e
JoaquinMorales <joaco_14@hotmail.com>
2011-05-24 03:53:49 -0400

    se agrega la direccion donde quiere recivir su suldo el empleado

--- src/datos/RegistroEmpleadoDBHelper.java
+++ src/datos/RegistroEmpleadoDBHelper.java
@@ -11,7 +11,7 @@ package datos;
  */
 public class RegistroEmpleadoDBHelper {
 
-     public static String registrarEmpleado(int idEmpleado,String tipoEmpleado, String nombreCompleto, int telefono, String direccion, String correo, double sueldoFijo, String formaDePago){
+     public static String registrarEmpleado(int idEmpleado,String tipoEmpleado, String nombreCompleto, int telefono, String direccion, String correo, double sueldoFijo, String formaDePago, String destinoPago){
         String consulta = "Insert into empleado (id, nombre_completo, telefono, direccion, correo_electronico, tipo_de_pago, sueldo_fijo, aporte_jubilacion,tipo_empleado, porcentaje_comision)Values ('" + idEmpleado + "','" + nombreCompleto + "','" + telefono + "','" + direccion + "','"+ correo + "','" + formaDePago+"','" + sueldoFijo+"', -1,'"+tipoEmpleado+"', -1)";
         //consulta = "Insert into empleado (id, nombre_completo, telefono, direccion, correo_electronico, tipo_de_pago, sueldo_fijo, aporte_jubilacion,tipo_empleado, porcentaje_comision)Values ('" + this.idEmpleado + "','" + this.nombreCompleto + "','" + this.telefono + "','" + this.direccion + "','"+ this.correoElectronico + "','" + this.formaDePago+"','" + this.sueldoFijo+"', '" + this.aporteJubilacion+"','"+Empleado.EmpleadoConComision+"', '"+this.porcentajeComision+"')";
         try {
